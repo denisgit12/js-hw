ce3e44cc75fe5ea94feb5cb5f06f0e9fcc4ff7f2
´denisgit12´ <yaholnykd@gmail.com>
2024-08-06 22:35:03 +0200

    add hw-1 js

--- Hw-1/scripts/main.js
+++ Hw-1/scripts/main.js
@@ -23,3 +23,4 @@ let firstName = prompt("What is your name?");
 let lastName = prompt("What is your last name?");
 let middleName = prompt("What is your middle name?");
 console.log(`${lastName} ${firstName} ${middleName}`)
+
