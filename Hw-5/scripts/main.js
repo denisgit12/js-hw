0400caa4518a121a827522b31410f122b9da6cfe
´denisgit12´ <yaholnykd@gmail.com>
2024-08-15 22:19:10 +0200

    add hw-5js

--- Hw-5/scripts/main.js
+++ Hw-5/scripts/main.js
@@ -168,6 +168,6 @@ exchange = (sumUAH, currencyValues, exchangeCurrency) => {
         }
     }
     return num;
-}
+};
 
 console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'EUR'));
